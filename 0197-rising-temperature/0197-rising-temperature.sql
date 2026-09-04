c56a6cc4fa39fca4d4d24351f40180bfb0433e81
Kratika Jain <k.jain1433@gmail.com>
2026-09-04 19:51:15 +0530

    Time: 491 ms (69.35%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,12 @@
+SELECT id AS Id
+FROM (
+    SELECT 
+        id,
+        recordDate,
+        temperature,
+        LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
+        LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp
+    FROM Weather
+) t
+WHERE recordDate = DATE_ADD(prev_date, INTERVAL 1 DAY)
+  AND temperature > prev_temp;
